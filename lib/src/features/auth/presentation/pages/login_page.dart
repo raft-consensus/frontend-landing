8058b0e12336585f55c35386d8c8ccd7adb4708f
Andrés Hidrobo <andresmhidroboe@gmail.com>
2026-07-27 14:39:47 -0500

    Fix: Remove unused import in login_page.dart
    Removed unused import for 'dart:html'.

--- lib/src/features/auth/presentation/pages/login_page.dart
+++ lib/src/features/auth/presentation/pages/login_page.dart
@@ -1,5 +1,3 @@
-import 'dart:html' as html;
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:frontend_landing/src/core/network/api_client.dart';
